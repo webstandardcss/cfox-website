f54505981bd3560a82479e58634a7e3f087b2368
Trey Brister <webstandardcss@gmail.com>
2020-09-24 08:42:46 -0500

    start header and footer and setup php shorttags on index.php

---
+++ inc/footer.inc.php
@@ -0,0 +1,26 @@
+
+<?php include "inc/contact.inc.php"; ?>
+<!-- For a Legal Consultation <a href="ConsultationForm.php">(click here)</a>. -->              
+<hr />
+<?php include "inc/locations.inc.php"; ?> <!-- end #mainContent ===--></div>
+<!-- TemplateEndEditable -->
+<p><!-- This clearing element should immediately follow the #mainContent div in order to force the #container div to contain all child floats=============================================================== --> &nbsp; &nbsp; &nbsp;<img id="dallasskyline" longdesc="../Dallas-Skyline.jpg" src="/images/Dallas-Skyline.jpg" alt="Dallas Skyline" width="268" height="88" align="bottom" /><img longdesc="../fort_worth_downtown.jpg" src="/images/fort_worth_downtown.jpg" alt="Fort Worth" hspace="25" width="140" height="88" align="bottom" /><br class="clearfloat" /></p>
+<div class="footer">
+<p><em>Nothing contained in this website is intended to create an offer for legal services. No attorney/ client relationship exists as a result of an individual or entity reading or utilizing the contents of these pages. The purpose of this site is purely informational. If you want legal counsel or advice, you should call a competent and licensed attorney. The Law Office of Christina Fox is a debt relief agency helping people to file for bankruptcy relief under the bankruptcy code.</em></p>
+<!-- end #footer --></div>
+<!-- end #container --></div>
+</body>
+</html>
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-26327040-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
---
+++ inc/header.inc.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html>
+	<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+
+<!--Meta Data for SEO-->
+
+<?php include "inc/meta.inc.php"; ?>
+<meta name="description" content="The Law Office of Christina Fox is serving the Dallas Fort Worth Metroplex handling consumer and business bankruptcy, accident and personal injury, criminal law, traffic tickets and warrants in Tarrant county and surrounding areas. ">
+<meta name="keywords" content="traffic ticket, citation, summons, traffic violations, fort worth traffic ticket, traffic ticket attorney, attorney, lawyer, texas, defense attorney, speeding, stop sign, no insurance, reckless driving, traffic ticket lawyer, traffic ticket court, traffic court, fight traffic ticket, traffic ticket dismissal, fighting traffic ticket, class c misdemeanor, tarrant county,Hurst, Euless, Bedford, HEB,tarrant, Ft. Worth lawyer, Fort Worth lawyer, Attorney, Attorneys, Certified, Law, Lawyer, Lawyers, family lawyer, divorce lawyer, custody lawyer, child support lawyer, Visitation lawyer, Arlington, Mid-Cities traffic ticket lawyer, Hurst lawyer, Euless lawyer, Bedford lawyer, Grapevine lawyer, Haltom City, Richland Hills lawyer, North Richland Hills lawyer, Mansfield, Keller, Southlake, southlake traffic violation, Colleyville traffic tickets, colleyville bankruptcy attorney, colleyville bankruptcy lawyer, colleyville personal injury attorney, colleyville wrongful death lawyer, colleyville family law lawyer, colleyville family law attorney, colleyville criminal defense lawyer, colleyville criminal defense attorney, Samson Park, Westworth Village, Forest Hills, Benbrook, Aledo, Azle, Saginaw, Watauga, Pantego, Crowley, Kennedale, Law, Association,texas, traffic tickets tx,christina fox, attorney,Fox,Family Law,divorces, child custody, child support, spousal support, modifications, enforcements, adoptions, marital agreements, Consumer Bankruptcy, Chapter7,Chapter13, family-based, Criminal Defense attorney, criminal defense lawyer, expunctions, non-disclosures, dwi, drug crimes, traffic citations, other, felonies, misdemeanors, Personal injury, Wrongful death, trusts,DFW, Fort Worth,Fort Worth bankruptcy attorney,Fort Worth bankruptcy lawyer,Fort Worth,Texas,attorney,lawyer,consumer bankruptcy,bankruptcy lawyer,business bankruptcy,bankruptcy attorney,Chapter 7,Chapter 11, Chapter 13, traffic violations, traffic tickets,">
+
+<title>The Law Office of Christina Fox is serving the Dallas Fort Worth Area</title>
+
+<link rel="shortcut icon" href="/images/ICON.jpg" />
+<script type="text/javascript" src="CFox.js"></script>
+<link href="CFox.css" rel="stylesheet" type="text/css" />
+
+		<link href="jquery-ui-1.8.16.custom.css" rel="stylesheet" />
+		<script src="http://code.jquery.com/jquery-1.7.1.js"></script>
+		<script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.13/jquery-ui.min.js"></script>
+		
+		<script>
+	$(function() {
+	
+		$( "#dialog" ).dialog({
+			width: 440,
+			modal: true,
+                        show: "blind",
+			hide: "explode",
+			buttons: {
+				Ok: function() {
+					$( this ).dialog ( "close" );
+				}
+			}
+		});
+	});
+		</script>
+		
+	</head>
+<div id="fb-root"></div>
+
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
+  fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));
+</script>
+
+<body class="thrColHybHdr" onload="MM_preloadImages('/images/rbso.gif','/images/Facebook logo.png','/images/Twitter.png','/images/LinkedIn.png');">
+<!--
+<div id="dialog" title="Welcome to the Law Office of Christina Fox">
+<p style="text-align:left" mce_style="text-align:left">On JUNE 1st, we MOVED to a larger facility in Colleyville and increased our staff to better support our clients' needs.</p>
+<blockquote>Our <i> <mark>NEW ADDRESS</mark></i> is:<br /><b>1205 HALL JOHNSON ROAD<br />COLLEYVILLE, TEXAS 76034</b></blockquote>
+<p style="text-align:left" mce_style="text-align:left">This office suite is located at the Tivoli Court Law Office off Colleyville Blvd.</p>
+</div>
+-->
+<div id="container"><!-- TemplateBeginEditable name="HeaderContent" --> <!-- my stuff again -->
+<div id="header">
+<table style="height: 250px;" border="0" align="center">
+<tbody>
+<tr style="vertical-align:bottom">
+
+<td>
+	<div class="fb-like" data-href="http://cfoxlawoffice.com" data-width="50" data-layout="button_count" data-action="like" data-show-faces="false" data-share="false">&nbsp;</div>
+</td>
+<td>&nbsp; &nbsp; &nbsp;</td>
+<td><a class="twitter-follow-button" href="https://twitter.com/cfoxlaw">Follow @cfoxlaw</a>
+<script type="text/javascript"><!--
+!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
+// --></script>
+</td>
+
+</tr>
+</tbody>
+</table>
+</div>
+<!-- TemplateEndEditable -->
+<div id="sidebarfont" class="curvebox">
+<table border="0">
+<tbody>
+<tr>
+<td colspan="2"><a onmouseover="MM_swapImage('home','','/images/rbso.gif',1)" onmouseout="MM_swapImgRestore()" href="http://www.cfoxlawoffice.com/index.html"><img id="home" src="/images/rbsStatic.gif" border="0" alt="Home" vspace="0" width="18" height="18" align="top" />&nbsp;&nbsp;<small>HOME</small><br /></a></td>
+</tr>
+<tr>
+<td colspan="2">
+<p><a onmouseover="MM_swapImage('profile','','/images/rbso.gif',1)" onmouseout="MM_swapImgRestore()" href="http://www.cfoxlawoffice.com/profile.html"><img id="profile" src="/images/rbsStatic.gif" border="0" alt="Attorney Profile" width="18" height="18" align="top" />&nbsp;&nbsp;<small>PROFILE</small></a></p>
+</td>
+</tr>
+<tr>
+<td colspan="2"><a onmouseover="MM_swapImage('practice_areas','','/images/rbso.gif',1)" onmouseout="MM_swapImgRestore()" href="http://www.cfoxlawoffice.com/practice_areas.html"><img id="practice_areas" src="/images/rbsStatic.gif" border="0" alt="Practice Areas" width="18" height="18" align="top" />&nbsp;&nbsp;<small>PRACTICE <br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;AREAS</small></a></td>
+</tr>
+<tr>
+<td colspan="2">
+<p><a onmouseover="MM_swapImage('affiliations','','/images/rbso.gif',1)" onmouseout="MM_swapImgRestore()" href="http://www.cfoxlawoffice.com/affiliations.html"><img id="affiliations" src="/images/rbsStatic.gif" border="0" alt="Affiliations" width="18" height="18" align="top" />&nbsp;&nbsp;<small>AFFILIATIONS</small></a></p>
+</td>
+</tr>
+<tr>
+<td colspan="2"><a onmouseover="MM_swapImage('awards','','/images/rbso.gif',1)" onmouseout="MM_swapImgRestore()" href="http://www.cfoxlawoffice.com/awards.html"><img id="awards" src="/images/rbsStatic.gif" border="0" alt="Awards" width="18" height="18" align="top" />&nbsp;&nbsp;<small>AWARDS</small></a></td>
+</tr>
+<tr>
+<td colspan="2">
+<p><a onmouseover="MM_swapImage('contactus','','/images/rbso.gif',1)" onmouseout="MM_swapImgRestore()" href="http://www.cfoxlawoffice.com/contact_us.html"><img id="contactus" src="/images/rbsStatic.gif" border="0" alt="Contact Us" width="18" height="18" align="top" />&nbsp;&nbsp;<small>CONTACT&nbsp;US</small></a></p>
+</td>
+</tr>
+<tr>
+<td colspan="2"><a onmouseover="MM_swapImage('payment','','/images/rbso.gif',1)" onmouseout="MM_swapImgRestore()" href="http://www.cfoxlawoffice.com/payment_options.html"><img id="payment" src="/images/rbsStatic.gif" border="0" alt="Payment Options" vspace="0" width="18" height="18" align="top" />&nbsp;&nbsp;<small>PAYMENT<br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;OPTIONS</small></a></td>
+</tr>
+<tr>
+<td colspan="2">
+<p><a onmouseover="MM_swapImage('blog','','/images/rbso.gif',1)" onmouseout="MM_swapImgRestore()" href="http://www.cfoxlawoffice.com/FoxLaw/" target="_blank"><img id="blog" src="/images/rbsStatic.gif" border="0" alt="Blog" vspace="0" width="18" height="18" align="top" />&nbsp;&nbsp;<small>FOX LAW<br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;BLOG</small></a></p>
+</td>
+</tr>
+</tbody>
+</table>
+</div>
+<div id="sidebar2" class="curvebox">
+<h4>&nbsp; &nbsp; &nbsp; Primary Practice Areas</h4>
+<p><img id="PracticePillars" class="fltlft" longdesc="Practice-Areas2.png" src="/images/Practice-Areas2.png" alt="primarypracticeareas" hspace="0" vspace="0" width="70" height="240" align="middle" /></p>
+<p>&nbsp;</p>
+<p>Wrongful Death/<br />Personal Injury</p>
+<p>Family Law</p>
+<p>Bankruptcy</p>
+<p>Criminal Defense</p>
+<p><img longdesc="/images/FoxLawLogo-a.png" src="/images/FoxLawLogo-a.png" alt="justice" hspace="25" vspace="10" width="61" height="49" /></p>
+</div>
+<!-- TemplateBeginEditable name="MainContent" -->
+<div id="mainContent">
