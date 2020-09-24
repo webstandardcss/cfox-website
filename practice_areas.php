c39ae44368c665427029b4b7952776dd5f44f01e
Trey Brister <webstandardcss@gmail.com>
2020-09-24 10:59:51 -0500

    modular pages include files

--- practice_areas.php
+++ practice_areas.php
@@ -1,151 +1,17 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<!--Meta Data for SEO-->
-
-<meta name="description" content="Christina Fox Attorney at law office serving the Dallas Fort Worth Metroplex. Handling family law, consumer bankruptcy, business bankruptcy, accident, personal injury, wrongful death and criminal law cases as a Dallas lawyer, Fort Worth lawyer, Richland Hills lawyer, North Richland Hills lawyer, Colleyville lawyer, Grapevine lawyer, Hurst lawyer, Euless lawyer, Bedford lawyer and D/FW metroplex lawyer. Covering fort worth, dallas, colleyville, southlake, arlington, keller, hurst, euless, bedford, heb, Tarrant County and Dallas County clients.">
-
-<meta name="keywords" content="Hurst, Euless, Bedford, HEB,tarrant, Ft. Worth lawyer, Fort Worth lawyer, Attorney, Attorneys, Certified, Law, Lawyer, Lawyers, family lawyer, divorce lawyer, custody lawyer, child support lawyer, Visitation lawyer, Arlington, Mid-Cities lawyer, Hurst lawyer, Euless lawyer, Bedford lawyer, Grapevine lawyer, Haltom City, Richland Hills lawyer, North Richland Hills lawyer, Mansfield, Keller, Southlake, Colleyville, colleyville bankruptcy attorney, colleyville bankruptcy lawyer, colleyville personal injury attorney, colleyville wrongful death lawyer, colleyville family law lawyer, colleyville family law attorney, colleyville criminal defense lawyer, colleyville criminal defense attorney, Samson Park, Westworth Village, Forest Hills, Benbrook, Aledo, Azle, Saginaw, Watauga, Pantego, Crowley, Kennedale, Law, Association,texas, tx,christina fox, attorney,Fox,Family Law,divorces, child custody, child support, spousal support, modifications, enforcements, adoptions, marital agreements, Consumer Bankruptcy, Chapter7,Chapter13,Chapter11, family-based, Criminal Defense attorney, criminal defense lawyer, expunctions, non-disclosures, dwi, drug crimes, traffic citations, other, felonies, misdemeanors, Personal injury, Wrongful death, trusts,DFW, Fort Worth,Fort Worth bankruptcy attorney,Fort Worth bankruptcy lawyer,Fort Worth,Texas,attorney,lawyer,consumer bankruptcy,bankruptcy lawyer,bankruptcy attorney,Chapter 7, Chapter 13, Chapter 11">
-
-<!-- TemplateBeginEditable name="doctitle" -->
-
-<title>LAW OFFICE of CHRISTINA FOX</title>
-<!-- TemplateEndEditable -->
-<!-- My stuff -->
-<script type="text/javascript">
-		window.onload=function(){
-		    Nifty("div.curvebox");
-		}
-</script>
-
-<!-- End of My stuff -->
-<!--<style type="text/css"> 
-   @import url(/CFox.css);
-</style> -->
-<link href="CFox.css" rel="stylesheet" type="text/css" media="all" />
-
-<link href="/images/Menu Sidebar.css" rel="stylesheet" type="text/css" />
-<script type="text/javascript">
-
-function MM_swapImgRestore() { //v3.0
-  var i,x,a=document.MM_sr; for(i=0;a&&i<a.length&&(x=a[i])&&x.oSrc;i++) x.src=x.oSrc;
-}
-function MM_preloadImages() { //v3.0
-  var d=document; if(d.images){ if(!d.MM_p) d.MM_p=new Array();
-    var i,j=d.MM_p.length,a=MM_preloadImages.arguments; for(i=0; i<a.length; i++)
-    if (a[i].indexOf("#")!=0){ d.MM_p[j]=new Image; d.MM_p[j++].src=a[i];}}
-}
-
-function MM_findObj(n, d) { //v4.01
-  var p,i,x;  if(!d) d=document; if((p=n.indexOf("?"))>0&&parent.frames.length) {
-    d=parent.frames[n.substring(p+1)].document; n=n.substring(0,p);}
-  if(!(x=d[n])&&d.all) x=d.all[n]; for (i=0;!x&&i<d.forms.length;i++) x=d.forms[i][n];
-  for(i=0;!x&&d.layers&&i<d.layers.length;i++) x=MM_findObj(n,d.layers[i].document);
-  if(!x && d.getElementById) x=d.getElementById(n); return x;
-}
-
-function MM_swapImage() { //v3.0
-  var i,j=0,x,a=MM_swapImage.arguments; document.MM_sr=new Array; for(i=0;i<(a.length-2);i+=3)
-   if ((x=MM_findObj(a[i]))!=null){document.MM_sr[j++]=x; if(!x.oSrc) x.oSrc=x.src; x.src=a[i+2];}
-}
-
-function ToggleVisibility(image, element) {
-	// Find the image
-	var image = document.getElementById(image)
-	//Find the element to hide/unhide
-	var element = document.getElementById(element)
-
-	//Check the element's current state
-	if (element.style.display == "none"){
-		//If hidden, unhide it
-		element.style.display = "block"
-		image.src = "/images/open.png"
-	}
-	else
-	{
-		// If not hidden, hide it
-		element.style.display = "none"
-		image.src = "/images/close.png"
-	}
-}
-</script>
-<!-- TemplateBeginEditable name="head" -->
-<link rel="shortcut icon" href="/images/ICON.jpg" />
-<!-- TemplateEndEditable -->
-
-</head>
-<body class="thrColHybHdr" onload="MM_preloadImages('/images/rbso.gif','/images/Facebook logo.png','/images/Twitter.png','/images/LinkedIn.png')">
-<div id="container"><!-- TemplateBeginEditable name="HeaderContent" --> <!-- my stuff again -->
-<div id="header">
-<!--=====<ul id="albumlist" class="onclick=&quot;href=&quot;http://www.facebook.com/cfoxlaw&quot;&quot;">
-<li><img src="/images/facebook_logo.png" border="0" alt="LIKE CFox Law on Facebook" hspace="235" vspace="200px" width="31" height="30" /></li>
-<li><img src="/images/LinkedIn.png" border="0" alt="Join Me on LinkedIn" hspace="240" vspace="200" width="38" height="35" /></li>
-<li><img src="/images/Twitter.png" border="0" alt="Follow Me on Twitter" hspace="245" vspace="200" width="38" height="35" /></li>
-</ul>=====-->
-</div>
-<!--</script>
-i removed-->     <!-- end #header<img src="../CFox Law No Banner Med.png" mce_src="../CFox Law No Banner Med.png" alt="Christina Fox, Attorney at Law" name="christinafox" width="1" height="240" align="top" id="christinafox" longdesc="../CFox Law No Banner Med.png" /> -->  <!-- TemplateEndEditable -->
-<div id="sidebarfont" class="curvebox">
-<table>
-<tr>
-<td colspan=2>
-<a onmouseover="MM_swapImage('home','','/images/rbso.gif',1)" onmouseout="MM_swapImgRestore()" href="http://www.cfoxlawoffice.com/index.html"><img id="home" src="/images/rbsStatic.gif" border="0" alt="Home" vspace="0" width="18" height="18" align="top" />&nbsp;&nbsp;<small>HOME</small><br/></a>
-</td>
-</tr>
-<tr>
-<td colspan=2>
-<p><a onmouseover="MM_swapImage('profile','','/images/rbso.gif',1)" onmouseout="MM_swapImgRestore()" href="http://www.cfoxlawoffice.com/profile.html"><img id="profile" src="/images/rbsStatic.gif" border="0" alt="Attorney Profile" width="18" height="18" align="top" />&nbsp;&nbsp;<small>PROFILE</small></a></p>
-</td>
-</tr>
-<tr>
-<td colspan=2>
-<a onmouseover="MM_swapImage('practice_areas','','/images/rbso.gif',1)" onmouseout="MM_swapImgRestore()" href="http://www.cfoxlawoffice.com/practice_areas.html"><img id="practice_areas" src="/images/rbsStatic.gif" border="0" alt="Practice Areas" width="18" height="18" align="top" />&nbsp;&nbsp;<small>PRACTICE
-<br/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;AREAS</small></a>
-</td>
-</tr>
-<tr>
-<td colspan=2>
-<p><a onmouseover="MM_swapImage('affiliations','','/images/rbso.gif',1)" onmouseout="MM_swapImgRestore()" href="http://www.cfoxlawoffice.com/affiliations.html"><img id="affiliations" src="/images/rbsStatic.gif" border="0" alt="Affiliations" width="18" height="18" align="top" />&nbsp;&nbsp;<small>AFFILIATIONS</small></a></p>
-</td>
-</tr>
-<tr>
-<td colspan=2>
-<a onmouseover="MM_swapImage('awards','','/images/rbso.gif',1)" onmouseout="MM_swapImgRestore()" href="http://www.cfoxlawoffice.com/awards.html"><img id="awards" src="/images/rbsStatic.gif" border="0" alt="Awards" width="18" height="18" align="top" />&nbsp;&nbsp;<small>AWARDS</small></a>
-</td>
-</tr>
-<tr>
-<td colspan=2>
-<p><a onmouseover="MM_swapImage('contactus','','/images/rbso.gif',1)" onmouseout="MM_swapImgRestore()" href="http://www.cfoxlawoffice.com/contact_us.html"><img id="contactus" src="/images/rbsStatic.gif" border="0" alt="Contact Us" width="18" height="18" align="top" />&nbsp;&nbsp;<small>CONTACT&nbsp;US</small></a></p>
-</td>
-</tr>
-<tr>
-<td colspan=2>
-<a onmouseover="MM_swapImage('payment','','/images/rbso.gif',1)" onmouseout="MM_swapImgRestore()" href="http://www.cfoxlawoffice.com/payment_options.html"><img id="payment" src="/images/rbsStatic.gif" border="0" alt="Payment Options" vspace="0" width="18" height="18" align="top"/>&nbsp;&nbsp;<small>PAYMENT<br/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;OPTIONS</small></a>
-</td>
-</tr>
-<tr>
-<td colspan=2>
-<p><a onmouseover="MM_swapImage('blog','','/images/rbso.gif',1)" onmouseout="MM_swapImgRestore()" href="http://www.cfoxlaw.wordpress.com/" target="_blank"><img id="blog" src="/images/rbsStatic.gif" border="0" alt="Blog" vspace="0" width="18" height="18" align="top"/>&nbsp;&nbsp;<small>FOX LAW<br/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;BLOG</small></a></p>
-</td>
-</tr>
-</table>
-</div>
-<div id="sidebar2" class="curvebox">
-<h4>&nbsp; &nbsp; &nbsp; Primary Practice Areas</h4>
-<p><img id="PracticePillars" class="fltlft" longdesc="Practice-Areas2.png" src="/images/Practice-Areas2.png" alt="primarypracticeareas" hspace="0" vspace="0" width="70" height="240" align="middle" /></p>
-<p>&nbsp;</br>&nbsp;</p>
-<p>Wrongful Death/<br />Personal Injury</p>
-<p>Family Law</p>
-<p>Consumer<br/>Bankruptcy</p>
-<p>Business<br/>Bankruptcy</p>
-<p>Criminal Defense</p>
-<p><img longdesc="/images/FoxLawLogo-a.png" src="/images/FoxLawLogo-a.png" alt="justice" hspace="25" vspace="10" width="61" height="49" /></p>
-</div>
-<!-- TemplateBeginEditable name="MainContent" -->
-<div id="mainContent">
+<?php 
+include "inc/config.inc.php";
+$pageTitle="Primary Practice Areas";
+$pageHeader = <<<EOT
+    <title>$pageTitle - $firmName is serving the $firmMetros areas</title>
+    <meta name="description" content="$firmName $pageTitle" />
+    <meta name="keywords" content="$pageTitle, $firmKeywords, $firmCities" />
+EOT;
+$pageFooter = <<<EOT
+EOT;
+?>
+<?php include "inc/header.inc.php"; ?>
 <h3>
-<p>Primary Practice Areas</p>
+	<p>Primary Practice Areas</p>
 </h3>
 <p class="txt_blue"><img id="PersonalInjuryWrongfulDeath" src="/images/open.png" alt="personal injury wrongful death" width="20" height="15" onclick="ToggleVisibility('PersonalInjuryWrongfulDeath', 'personal_injury_wrongful_death')" /><strong>&nbsp; &nbsp;Personal Injury/ Wrongful Death</strong></p>
 
@@ -167,14 +33,4 @@ i removed-->     <!-- end #header<img src="../CFox Law No Banner Med.png" mce_sr
 
 <div id="criminal_defense"><p>Criminal punishments for crimes are outlined in the Texas State Penal Code. For each crime committed such as DWIs, Drug Crimes, Other Felonies and Misdemeanors, there are a variety of defenses a defendant may use to prove his innocence, argue for the case to be dismissed or have his sentence reduced. For the criminal defendant to be found guilty the prosecuting attorney must prove the defendant is guilty beyond a reasonable doubt.  If you've been accused of a crime, it is important that you get competent legal counsel, such as Christina Fox, attorney at law, as soon as possible.<br /><br /> Call or email now for your consultation:</p></div>
 
-<?php include "inc/contact.inc.php"; ?>
-<p>&nbsp;</p>
-<!-- end #mainContent --></div>
-<!-- TemplateEndEditable -->
-<p><!-- This clearing element should immediately follow the #mainContent div in order to force the #container div to contain all child floats --> <img id="dallasskyline" longdesc="../Dallas-Skyline.jpg" src="/images/Dallas-Skyline.jpg" alt="Dallas Skyline" width="268" height="88" align="bottom" /><img longdesc="../fort_worth_downtown.jpg" src="/images/fort_worth_downtown.jpg" alt="Fort Worth" hspace="25" width="140" height="88" align="bottom" /><br class="clearfloat" /></p>
-<div class="footer">
-<p><em>Nothing contained in this website is intended to create an offer for legal services. No attorney/ client relationship exists as a result of an individual or entity reading or utilizing the contents of these pages. The purpose of this site is purely informational. If you want legal counsel or advice, you should call a competent and licensed attorney. The law office of Christina Fox is a debt relief agency helping people to file for bankruptcy relief under the bankruptcy code.</em></p>
-<!-- end #footer --></div>
-<!-- end #container --></div>
-</body>
-</html>
+<?php include "inc/footer.inc.php"; ?>
